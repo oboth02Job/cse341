8efd53966ab4d7c073c8cb282be49ec2896a5311
Oboth03 <joboboth601@gmail.com>
2026-05-16 15:33:25 +0300

    .

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require("express")
 
+const dotenv = require("dotenv").config();
+
+const mongodb = require("./cse-341-project1/data/database")
+
 const app = express()
 
 const port = process.env.PORT || 3000;
 
-app.use("/", require("./routes"))
+app.use("/", require("./cse-341-project1/data"))
 
+mongodb.initDb((err) => {
+    if (err) {
+    console.log(err)
+    }
+    else {
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+  console.log(`Database is listening and server is running on port ${port}`);
+});
+    }
 })
+
