8c7e9955d7f9dc5d0fb1951dd7a7de3b6bd817ca
Oboth03 <joboboth601@gmail.com>
2026-05-23 20:31:37 +0300

    installed swagger

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const ObjectId = require("mongodb").ObjectId
 const mongodb = require("../data/database")
 
 const getAll = async (req, res) => {
+  //#swagger.tags=["Users"]
     const result = mongodb
         .getDatabase()
         .collection("my databases")
@@ -13,14 +14,77 @@ const getAll = async (req, res) => {
 }
 
 const getSingle = async (req, res) => {
-    const userId = new ObjectId(req.params.id)
-    const user = await mongodb
-        .getDatabase()
-        .collection("my databases")
-        .findOne({ _id: userId })
+  //#swagger.tags=["Users"]
+  const userId = new ObjectId(req.params.id);
+  const user = await mongodb
+    .getDatabase()
+    .collection("my databases")
+    .findOne({ _id: userId });
 
-    res.setHeader("Content-Type", "application/json")
-    res.status(200).json(user)
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).json(user);
 }
 
-module.exports = {getAll, getSingle}
+const createUser = async (req, res) => {
+  //#swagger.tags=["Users"]
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const response = await mongodb
+    .getDatabase()
+    .collection("my databases")
+    .insertOne(user);
+  if (response.acknowledged) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.error || "Some error occurred while updating user");
+  }
+}
+
+const updateUser = async (req, res) => {
+  //#swagger.tags=["Users"]
+  const userId = new ObjectId(req.params.id);
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const response = await mongodb
+    .getDatabase()
+    .collection("my databases")
+    .replaceOne({ _id: userId }, user);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.error || "Some error occurred while updating user");
+  }
+};
+
+const deleteUser = async (req, res) => {
+  //#swagger.tags=["Users"]
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb
+    .getDatabase()
+    .collection("my databases")
+    .deleteOne({ _id: userId });
+  if (response.deletedCount) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.error || "Some error occurred while updating user");
+  }
+};
+
+
+module.exports = { getAll, getSingle, createUser, updateUser, deleteUser };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router()
 
+router.use("/", require("./swagger"));
+
 router.get("/", (req, res) => {
     res.send("Hello World")
 })
 
-router.use("/users", require("../routes/users"))
+router.use("/users", require("./users"))
 
 module.exports = router
